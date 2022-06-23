4f675075504755577360a46068f790b1173cb480
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-23 10:35:24 +0530

    Add files via upload

---
+++ slicing_python_practise.py
@@ -0,0 +1,41 @@
+a='Microsoft'
+# print(a[2:]) # crosoft
+# print(a[3:]) # rosoft
+# print(a[:]) # Microsoft
+# print(a[1:]) # icrosoft
+# print(a[4:]) # osoft
+# print(a[5:]) # soft
+# print(a[6:]) # oft
+# print(a[7:]) # ft
+# print(a[8:]) # t
+
+# b='python'
+# print(b[:]) # python
+# print(b[:1]) # p
+# print(b[:2]) # py
+# print(b[:3]) # pyt
+# print(b[:4]) # pyth
+# print(b[:5]) # pytho
+# print(b[:6]) # python
+
+# c='visualcode'
+# print(c[1:1]) # Blankline
+# print(c[2:2]) # Blankline
+# print(c[3:3]) # Blankline
+# print(c[4:4]) # Blankline
+# print(c[5:5]) # Blankline
+# print(c[6:6]) # Blankline
+# print(c[7:7]) # Blankline
+# print(c[8:8]) # Blankline
+# print(c[9:9]) # Blankline
+
+d='logitech'
+print(d[1:7]) # ogitec
+print(d[2:7]) # gitec
+print(d[3:7]) # itec
+print(d[4:7]) # tec
+print(d[5:7]) # ec
+print(d[6:7]) # c
+print(d[7:7]) # blankline
+
+
